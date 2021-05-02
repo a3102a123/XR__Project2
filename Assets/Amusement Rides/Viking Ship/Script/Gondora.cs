873a362a8bb8c1c04ade1806853b9d0fcc3e1d7c
ray17606 <44019584+ray17606@users.noreply.github.com>
2021-05-02 18:12:19 +0800

    Add files via upload
    can control ship by controller

--- Assets/Amusement Rides/Viking Ship/Script/Gondora.cs
+++ Assets/Amusement Rides/Viking Ship/Script/Gondora.cs
@@ -9,12 +9,14 @@ public class Gondora : MonoBehaviour
     private Vector3 m_rotateAxis;    //旋转轴
     private float w = 0;                //角速度
     public GameObject ship;
+    private int goleft = 0;
     // Use this for initialization
     void Start()
     {
         //求出旋转轴
-        m_rotateAxis = Vector3.Cross(ship.transform.position - m_anchor.transform.position, Vector3.right);
+        m_rotateAxis = Vector3.Cross(ship.transform.position - m_anchor.transform.position, Vector3.left);
     }
+
     void DoPhysics()
     {
         float r = Vector3.Distance(m_anchor.transform.position, ship.transform.position);
@@ -34,10 +36,31 @@ public class Gondora : MonoBehaviour
         float thelta = w * Time.deltaTime * 180.0f / Mathf.PI;
         //绕圆点m_ahchor的旋转轴m_rotateAxis旋转thelta角度
         ship.transform.RotateAround(m_anchor.transform.position, m_rotateAxis, thelta);
+
     }
+    
+    void SwingControl()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.2f)
+        {
+            ship.transform.RotateAround(m_anchor.transform.position, m_rotateAxis, OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) * Time.deltaTime * 10);
+            if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) == 1.0f)
+            {
+                goleft = 1;
+            }
+        }
+
+       if (goleft == 1 && OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) == 0.0f)
+        {
+            ship.transform.RotateAround(m_anchor.transform.position, m_rotateAxis, -1 * 1.0f* Time.deltaTime * 10);
+
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        DoPhysics();
+        ///DoPhysics();
+        SwingControl();
     }
 }
