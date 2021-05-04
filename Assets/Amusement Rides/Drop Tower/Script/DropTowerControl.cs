39f63ebf7288e6fc471597868a4fec46b7ddc9f1
ray17606 <44019584+ray17606@users.noreply.github.com>
2021-05-05 00:32:06 +0800

    UI color change

--- Assets/Amusement Rides/Drop Tower/Script/DropTowerControl.cs
+++ Assets/Amusement Rides/Drop Tower/Script/DropTowerControl.cs
@@ -4,89 +4,52 @@ using UnityEngine;
 
 public class DropTowerControl : MonoBehaviour
 {
+    public GameObject controller;
     public GameObject chair;
     public GameObject energybar;
     public GameObject emptybar;
-    public OVRGrabbable activedevice;
-    public OVRGrabbable reward;
     private Vector3 start_position;
     private Vector3 energybarlength;
-    private int release;
-    private int toomuchpower;
-    public int active;
-    public int end;
+    private int visible;
     // Start is called before the first frame update
     void Start()
     {
         start_position = new Vector3(chair.transform.position.x, chair.transform.position.y, chair.transform.position.z);
         energybarlength = new Vector3(energybar.transform.localScale.x, energybar.transform.localScale.y, energybar.transform.localScale.z);
-        release = 0;
-        active = 0;
-        end = 0;
-        toomuchpower = 0;
+        visible = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (activedevice.isGrabbed == true)
+        if (chair.transform.position.y > 15)
         {
-            active = 1;
+            chair.transform.position = start_position;
+            energybar.transform.localScale = energybarlength;
         }
 
-        if (reward.isGrabbed == true)
+        if (OVRInput.Get(OVRInput.Button.Two) && OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.2f)
         {
-            end = 1;
-            Invoke("GameEnd", 8.0f);
+            chair.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            energybar.transform.localScale += new Vector3(0, 0.05f, 0);
+            visible = 0;
+            energybar.SetActive(true);
+            emptybar.SetActive(true);
         }
 
-        if (active == 1)
+        else if (OVRInput.GetUp(OVRInput.Button.Two))
         {
-            if (energybar.transform.localScale.y > 2 * emptybar.transform.localScale.y)
-            {
-                energybar.transform.localScale = energybarlength;
-                chair.transform.Translate(Vector3.down * Time.deltaTime * 10.0f, Space.World);
-                toomuchpower = 1;
-            }
-
-            if (OVRInput.Get(OVRInput.Button.Two) && OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.2f && chair.transform.position.y < 6.0f && toomuchpower == 0)
-            {
-                chair.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-                energybar.transform.localScale += new Vector3(0, 0.09f, 0);
-                release = 0;
-
-                energybar.SetActive(true);
-                emptybar.SetActive(true);
-            }
-
-            else if (OVRInput.GetUp(OVRInput.Button.Two))
-            {
-                release = 1;
-                toomuchpower = 0;
-            }
-
-            if (release == 1 && energybar.transform.localScale.y > 0.0f)
-            {
-                energybar.transform.localScale -= new Vector3(0, 0.3f, 0);
-            }
-            if ((release == 1 || toomuchpower == 1) && chair.transform.position.y > start_position.y)
-            {
-                chair.transform.Translate(Vector3.down * Time.deltaTime * 4.0f, Space.World);
-            }
-
-            void OnTriggerEnter(Collider aaa)
-            {
-                if (aaa.gameObject.tag == "reward")
-                {
-                    Destroy(aaa.gameObject);
-                }
-            }
+            visible = 1;
         }
-        
+
+        if (visible == 1 && energybar.transform.localScale.y > 0.0f)
+        {
+            energybar.transform.localScale -= new Vector3(0, 0.15f, 0);
+        }
+        if (visible == 1 && chair.transform.position.y > start_position.y)
+        {
+            chair.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+        }
     }
 
-    void GameEnd()
-    {
-        active = 0;
-    }
 }
